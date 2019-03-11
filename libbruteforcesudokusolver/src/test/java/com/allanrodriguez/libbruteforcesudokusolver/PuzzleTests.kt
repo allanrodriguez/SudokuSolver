70b4b19748b6999ebe6cb623e26de7f70fc363df
Allan Rodriguez <allanr1@uci.edu>
2019-03-10 22:23:12 -0700

    Added unit tests for libbruteforcesudokusolver.

---
+++ libbruteforcesudokusolver/src/test/java/com/allanrodriguez/libbruteforcesudokusolver/PuzzleTests.kt
@@ -0,0 +1,303 @@
+package com.allanrodriguez.libbruteforcesudokusolver
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PuzzleTests {
+
+    @Test
+    fun `set throws IndexOutOfBoundsException when row is less than 0`() {
+        // Arrange
+        val invalidRow = -1
+        val col = 0
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[invalidRow, col] = value
+        }
+    }
+
+    @Test
+    fun `set throws IndexOutOfBoundsException when row is greater than 8`() {
+        // Arrange
+        val invalidRow = 9
+        val col = 0
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[invalidRow, col] = value
+        }
+    }
+
+    @Test
+    fun `set throws IndexOutOfBoundsException when col is less than 0`() {
+        // Arrange
+        val row = 0
+        val invalidCol = -1
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[row, invalidCol] = value
+        }
+    }
+
+    @Test
+    fun `set throws IndexOutOfBoundsException when col is greater than 8`() {
+        // Arrange
+        val row = 0
+        val invalidCol = 9
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[row, invalidCol] = value
+        }
+    }
+
+    @Test
+    fun `set sets puzzle cell at row and col to value`() {
+        // Arrange
+        val row = 0
+        val col = 0
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act
+        puzzle[row, col] = value
+
+        // Assert
+        Assertions.assertEquals(value, puzzle[row, col])
+    }
+
+    @Test
+    fun `set with Pair throws IndexOutOfBoundsException when row is less than 0`() {
+        // Arrange
+        val pairWithInvalidRow: Pair<Int, Int> = Pair(-1, 0)
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidRow] = value
+        }
+    }
+
+    @Test
+    fun `set with Pair throws IndexOutOfBoundsException when row is greater than 8`() {
+        // Arrange
+        val pairWithInvalidRow: Pair<Int, Int> = Pair(9, 0)
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidRow] = value
+        }
+    }
+
+    @Test
+    fun `set with Pair throws IndexOutOfBoundsException when col is less than 0`() {
+        // Arrange
+        val pairWithInvalidCol: Pair<Int, Int> = Pair(0, -1)
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidCol] = value
+        }
+    }
+
+    @Test
+    fun `set with Pair throws IndexOutOfBoundsException when col is greater than 8`() {
+        // Arrange
+        val pairWithInvalidCol: Pair<Int, Int> = Pair(0, 9)
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidCol] = value
+        }
+    }
+
+    @Test
+    fun `set with Pair sets puzzle cell at row and col to value`() {
+        // Arrange
+        val pair: Pair<Int, Int> = Pair(0, 0)
+        val value = 1
+        val puzzle = Puzzle()
+
+        // Act
+        puzzle[pair] = value
+
+        // Assert
+        Assertions.assertEquals(value, puzzle[pair])
+    }
+
+    @Test
+    fun `get throws IndexOutOfBoundsException when row is less than 0`() {
+        // Arrange
+        val invalidRow = -1
+        val col = 0
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[invalidRow, col]
+        }
+    }
+
+    @Test
+    fun `get throws IndexOutOfBoundsException when row is greater than 8`() {
+        // Arrange
+        val invalidRow = 9
+        val col = 0
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[invalidRow, col]
+        }
+    }
+
+    @Test
+    fun `get throws IndexOutOfBoundsException when col is less than 0`() {
+        // Arrange
+        val row = 0
+        val invalidCol = -1
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[row, invalidCol]
+        }
+    }
+
+    @Test
+    fun `get throws IndexOutOfBoundsException when col is greater than 8`() {
+        // Arrange
+        val row = 0
+        val invalidCol = 9
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[row, invalidCol]
+        }
+    }
+
+    @Test
+    fun `get with Pair throws IndexOutOfBoundsException when row is less than 0`() {
+        // Arrange
+        val pairWithInvalidRow: Pair<Int, Int> = Pair(-1, 0)
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidRow]
+        }
+    }
+
+    @Test
+    fun `get with Pair throws IndexOutOfBoundsException when row is greater than 8`() {
+        // Arrange
+        val pairWithInvalidRow: Pair<Int, Int> = Pair(9, 0)
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidRow]
+        }
+    }
+
+    @Test
+    fun `get with Pair throws IndexOutOfBoundsException when col is less than 0`() {
+        // Arrange
+        val pairWithInvalidCol: Pair<Int, Int> = Pair(0, -1)
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidCol]
+        }
+    }
+
+    @Test
+    fun `get with Pair throws IndexOutOfBoundsException when col is greater than 8`() {
+        // Arrange
+        val pairWithInvalidCol: Pair<Int, Int> = Pair(0, 9)
+        val puzzle = Puzzle()
+
+        // Act/Assert
+        Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            puzzle[pairWithInvalidCol]
+        }
+    }
+
+    @Test
+    fun `toString returns puzzle as string with all cells set to '_'`() {
+        // Arrange
+        val expectedPuzzleString: String =
+                "+-------+-------+-------+\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "+-------+-------+-------+\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "+-------+-------+-------+\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "| _ _ _ | _ _ _ | _ _ _ |\n" +
+                "+-------+-------+-------+\n"
+        val puzzle = Puzzle()
+
+        // Act
+        val actualPuzzleString: String = puzzle.toString()
+
+        // Assert
+        Assertions.assertEquals(expectedPuzzleString, actualPuzzleString)
+    }
+
+    @Test
+    fun `toString returns puzzle with solved cells as string with all cells set to their numbers`() {
+        // Arrange
+        val expectedPuzzleString: String =
+                "+-------+-------+-------+\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "+-------+-------+-------+\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "+-------+-------+-------+\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "| 1 1 1 | 1 1 1 | 1 1 1 |\n" +
+                "+-------+-------+-------+\n"
+        val puzzle = Puzzle()
+
+        // Setting all the puzzle's cells to 1
+        for (row: Int in 0..8) {
+            for (col: Int in 0..8) {
+                puzzle[row, col] = 1
+            }
+        }
+
+        // Act
+        val actualPuzzleString: String = puzzle.toString()
+
+        // Assert
+        Assertions.assertEquals(expectedPuzzleString, actualPuzzleString)
+    }
+}
